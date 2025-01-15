aa1e7304017898ffad7b992b60cc1051081954f5
giant-paw <giantpawbusiness@gmail.com>
2025-01-15 08:24:20 +0700

    membuat navigasi

---
+++ app/src/main/java/com/example/a08januari2025/ui/navigasi/DestinasiNavigasi.kt
@@ -0,0 +1,21 @@
+package com.example.a08januari2025.ui.navigasi
+
+interface DestinasiNavigasi {
+    val route: String
+    val titleRes: String
+}
+
+object DestinasiHome: DestinasiNavigasi{
+    override val route: String = "home"
+    override val titleRes: String = "Home"
+}
+
+object DestinasiInsert: DestinasiNavigasi{
+    override val route: String = "insert"
+    override val titleRes: String = "Insert"
+}
+
+object DestinasiDetail: DestinasiNavigasi{
+    override val titleRes: String = "detail"
+    override val route: String = "Detail"
+}
